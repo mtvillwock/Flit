5d2c7c437595cc14061ba17aa3d1d29c22e2d605
Matthew Villwock <mtvillwock@gmail.com>
2015-11-11 14:44:46 -0800

    use show to only show msg > 3 chars

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ angular.module("flit", [])
 .controller('MainCtrl', function ($scope) {
   $scope.message = "Hello";
 
-  $scope.messages = ["Foo", "Bar", "Baz"];
+  $scope.messages = ["Foo", "Bar", "Baz", "Sweet", "Cool", "Swag"];
 
 })

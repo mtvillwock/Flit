5b9d39ed9d8bc90326dfe64589eeb2bbc1ffeda5
Matthew Villwock <mtvillwock@gmail.com>
2015-11-11 14:48:05 -0800

    iterate over array of hashes

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ angular.module("flit", [])
 .controller('MainCtrl', function ($scope) {
   $scope.message = "Hello";
 
-  $scope.messages = ["Foo", "Bar", "Baz", "Sweet", "Cool", "Swag"];
+  $scope.messages = [
+  { to: 8675309, from: 1234567, body: "Hey buddy"},
+  { to: 8675309, from: 1234567, body: "Wat?"},
+  { to: 1234567, from: 8675309, body: "Great job dude"},
+  { to: 1234567, from: 8675309, body: "Hey!"},
+  ];
 
 })
